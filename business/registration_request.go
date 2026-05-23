f893bd6e6a433dd4ac89dc79d9b56611c3e29d01
phuchtq <taikhoanhoconl123@gmail.com>
2026-05-23 14:36:23 +0700

    feat(registration request): refactor create request

--- business/registration_request.go
+++ business/registration_request.go
@@ -576,7 +576,7 @@ func (r *registrationRequestService) CreateRegistrationRequest(req request.Creat
 		ProfileID:          ctx.Value("sub").(string),
 		RegisterRole:       role,
 		IdentityCode:       identityCode,
-		IdentityCardBlobID: strings.TrimSpace(req.IdentityCardBlobID),
+		IdentityCardBlobID: strings.TrimSpace(req.IdentityCardCloudinaryID),
 		AvatarBlobID:       strings.TrimSpace(req.AvatarBlobID),
 		Region:             req.Region,
 		FirstName:          firstName,
