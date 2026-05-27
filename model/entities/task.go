627d1bd702c499f0f7a1c3bd19076c98032401f6
phuchtq <taikhoanhoconl123@gmail.com>
2026-05-27 21:22:44 +0700

    feat(task proof): fix query get task proofs

--- model/entities/task.go
+++ model/entities/task.go
@@ -77,18 +77,19 @@ type TaskProof struct {
 }
 
 type TaskProofWithIsChildTask struct {
-	ID             string    `json:"id"`
-	TaskID         string    `json:"task_id"`
-	Description    string    `json:"description"`
-	ActorProfileID string    `json:"actor_profile_id"`
-	ActorAddress   string    `json:"actor_address"`
-	ImageBlobID    string    `json:"image_blob_id"`
-	ReviewedBy     *string   `json:"reviewed_by"`
-	AIEvaluation   string    `json:"ai_evaluation"`
-	AIReason       string    `json:"ai_reason"`
-	ReviewStatus   string    `json:"review_status"`
-	RawSubmitDate  string    `json:"raw_submit_date"`
-	CreatedAt      time.Time `json:"created_at"`
-	UpdatedAt      time.Time `json:"updated_at"`
-	IsChildTask    bool      `json:"is_child_task"`
+	ID                    string    `json:"id"`
+	TaskID                string    `json:"task_id"`
+	Description           string    `json:"description"`
+	ActorProfileID        string    `json:"actor_profile_id"`
+	ActorAddress          string    `json:"actor_address"`
+	ImageBlobID           string    `json:"image_blob_id"`
+	ImageCloudinaryBlobID string    `json:"image_cloudinary_blob_id"`
+	ReviewedBy            *string   `json:"reviewed_by"`
+	AIEvaluation          string    `json:"ai_evaluation"`
+	AIReason              string    `json:"ai_reason"`
+	ReviewStatus          string    `json:"review_status"`
+	RawSubmitDate         string    `json:"raw_submit_date"`
+	CreatedAt             time.Time `json:"created_at"`
+	UpdatedAt             time.Time `json:"updated_at"`
+	IsChildTask           bool      `json:"is_child_task"`
 }
